488e5034cab097a1acec64c59936126cde70deb0
Ilyas Tyumenev <tumenoff@mail.ru>
2023-05-04 08:54:07 +0300

    Initial Commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  Clima
+//
+//  Created by Ilyas Tyumenev on 04.05.2023.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
