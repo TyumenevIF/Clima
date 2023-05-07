bde95943862170002ae57d610fc62d9ac1ded557
Ilyas Tyumenev <tumenoff@mail.ru>
2023-05-07 11:48:00 +0300

    Used JSONDecoder

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Ilyas Tyumenev on 07.05.2023.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let weather: [Weather]
+    let main: Main
+}
+
+struct Weather: Decodable {
+    let description: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
